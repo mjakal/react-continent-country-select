a7f5be8c56472003d6a805d5589ea949d921d97d
Martin Jakal <mjakal@gmail.com>
2020-04-29 12:32:41 +0200

    fixed tests

--- src/ContinentCountrySelect.test.js
+++ src/ContinentCountrySelect.test.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import ContinentCountrySelect from 'index';
+import ContinentCountrySelect from './index';
 
 describe('ReactAsyncTable', () => {
   it('renders without crashing', () => {
-    const selectedCountries = {BE: true, NL: true, HR: true };
+    const selectedCountries = { BE: true, NL: true, HR: true };
     const onChange = selected => {
       return selected;
-    }
-    
+    };
+
     const wrapper = shallow(
-      <ContinentCountrySelect 
+      <ContinentCountrySelect
         selected={selectedCountries}
         onChange={onChange}
       />
     );
-    
+
     expect(wrapper).toHaveLength(1);
   });
 });
